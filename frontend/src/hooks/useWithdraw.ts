eacf072d2f6a718854a3c62e14a4de612c80fc52
ineff3 <maksymbochkarov@gmail.com>
2025-03-31 10:25:50 +0200

    Fix withdraw hook to send assets instead of shares

--- frontend/src/hooks/useWithdraw.ts
+++ frontend/src/hooks/useWithdraw.ts
@@ -1,5 +1,7 @@
 import { TokenPreview } from '@/types'
 import { getBasedContract } from '@/utils/getBaseContract'
+import { wagmiConfig } from '@/wagmi.config'
+import { readContracts } from '@wagmi/core'
 import { useAccount, useWaitForTransactionReceipt, useWriteContract } from 'wagmi'
 
 export const useWithdraw = () => {
@@ -10,13 +12,25 @@ export const useWithdraw = () => {
     hash: data,
   })
 
-  const withdraw = (
-    tokenPreview: TokenPreview,
-    amount: bigint,
-    options?: { onSuccess?: () => void; onError?: (error: Error) => void },
-  ) => {
+  const withdraw = async (tokenPreview: TokenPreview, amount: bigint) => {
     const baseContract = getBasedContract(tokenPreview)
-    writeContract({ ...baseContract, functionName: 'withdraw', args: [amount, account!, account!] }, options)
+    const result = await readContracts(wagmiConfig, {
+      contracts: [
+        {
+          ...baseContract,
+          functionName: 'convertToAssets',
+          args: [amount],
+        },
+      ],
+    })
+
+    if (!result) {
+      return
+    }
+
+    const assets = result?.[0].result
+
+    writeContract({ ...baseContract, functionName: 'withdraw', args: [assets!, account!, account!] })
   }
 
   return { withdraw, isTxLoading, isTxFinished }
